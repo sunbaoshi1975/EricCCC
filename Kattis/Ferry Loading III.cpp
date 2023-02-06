527e66efeebbf616a5ee0c5f81223829e898c51b
sunbaoshi <sunbaoshi1975@gmail.com>
2023-02-06 16:34:55 -0500

    Ferry Loading III comment

--- Kattis/Ferry Loading III.cpp
+++ Kattis/Ferry Loading III.cpp
@@ -1,4 +1,8 @@
-/* Wrong Answer ? */
+/* ticky: 
+    If there are more than n, those that have been waiting the longest are loaded.
+    So, ship may cross for opposite car w/o loading, 
+    just because car on other side arrived a bit ealier.
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -34,54 +38,50 @@ int main() {
         }
         int time = 0;
         int left = 1;
-        int nextLeft, nextRight;
-        // if need to change side in the first place?
-        if(side[left][0] > 0 && side[1 - left][0] == 0) {
-            time+=t;
-            left = 1 - left;
-        }
+        int next[2], space = n;
         while(side[0].size() > 0 || side[1].size() > 0) {
-            nextLeft = MAX_T;
-            nextRight = MAX_T;
+            // Who is next?
+            next[0] = MAX_T;
+            next[1] = MAX_T;
+            if(side[left].size() > 0) next[left] = side[left][0];
+            if(side[1 - left].size() > 0) next[1 - left] = side[1 - left][0];
+            if(next[left] <= next[1 - left] && next[left] < MAX_T) {
+                if(time < next[left]) {
+                    if(space < n) {
+                        time += (2 * t);
+                        space = n;
+                    }
+                    if(time < next[left]) time = next[left];
+                }
+            } else if(next[1 - left] < next[left]) {
+                // Change side
+                if(space < n) {
+                    time += t;
+                    if(time < next[1 - left]) time = next[1 - left];
+                } else {
+                    if(time < next[1 - left]) time = next[1 - left];
+                    time += t;
+                }
+                left = 1 - left;
+                space = n;
+            }
+
             // Go for a ride?
             if(side[left].size() > 0) {
                 if(side[left][0] <= time) {
-                    int space = n;
-                    while(side[left].size() > 0) {
-                        if(side[left][0] <= time && space > 0) {
-                            space--;
-                            cout << time + t << endl;
-                            side[left].pop_front();
-                        } else break;
+                    space--;
+                    cout << time + t << endl;
+                    side[left].pop_front();
+                    if(space == 0) {
+                        // Change side
+                        time += t;
+                        left = 1 - left;
+                        space = n;                        
                     }
-                    time+=t;
-                    left = 1 - left;
-                    continue;
-                } else {
-                    nextLeft = side[left][0];
                 }
-            }
-            // Need go empty?
-            if(side[1 - left].size() > 0) {
-                if(side[1 - left][0] <= time) {
-                    time+=t;
-                    left = 1 - left;
-                    continue;
-                } else {
-                    nextRight = side[1 - left][0];
-                }
-            }
-            // Wait at where I am until the next car appears,
-            // which could be on either bank.
-            if(nextLeft == MAX_T && nextRight == MAX_T) break;
-            if(nextLeft <= nextRight) {
-                time = nextLeft;
-            } else {
-                time = nextRight + t;
-                left = 1 - left;
             }
         }
-        cout << endl;
+        if(c) cout << endl;
     }
     return 0;
 }
